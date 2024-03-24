58c583c754a872f892da083d2f03b163b27b5098
BWbwchen <tim.chenbw@gmail.com>
2024-03-24 15:41:03 +0800

    [feat   ]: finish project 2 with compaction
    Signed-off-by: Bo-Wei Chen(BWbwchen) <tim.chenbw@gmail.com>

--- src/kv.rs
+++ src/kv.rs
@@ -6,7 +6,9 @@ use std::path::PathBuf;
 use std::{collections::HashMap, fs::File};
 use uuid::Uuid;
 
-use crate::{KvsError, Result};
+use crate::{KvsError, Result, DEFAULT_LOG_FILE};
+
+const MAX_LOG_UNCOMPACTED_BYTES: u64 = 1024 * 1024;
 
 /// The KvStore store the key-value database.
 ///
@@ -14,37 +16,45 @@ use crate::{KvsError, Result};
 ///
 /// Example:
 /// ```rust
-/// # use kvs::KvStore;
-/// let mut store = KvStore::new();
-/// store.set("key1".to_owned(), "value1".to_owned());
-/// //let value = store.get("key1".to_owned());
-/// //assert_eq!(value, Some("value1".to_owned()));
+/// # use kvs::{KvStore, Result};
 ///
+/// # fn main() -> Result<()> {
+/// let mut store = KvStore::open(kvs::DEFAULT_LOG_FILE)?;
+/// store.set("key1".to_owned(), "value1".to_owned());
+/// let value = store.get("key1".to_owned())?;
+/// assert_eq!(value, Some("value1".to_owned()));
+/// # Ok(())
+/// # }
+/// ```
 pub struct KvStore {
     // log writer
     logger: Logger,
     /// Key -> the index of latest serialized Cmd
     index: HashMap<String, CmdIdx>,
+    /// uncompacted size
+    uncompacted: u64,
 }
 
 struct CmdIdx {
     start: usize,
     len: usize,
+    cmd: Cmd, // cache the value here, If it is empty, it represend remove.
 }
 impl CmdIdx {
-    fn new(start: usize, len: usize) -> Self {
-        Self { start, len }
+    fn new(start: usize, len: usize, cmd: Cmd) -> Self {
+        Self { start, len, cmd }
     }
     fn default() -> Self {
-        Self::new(0, 0)
+        Self::new(0, 0, Cmd::Empty)
     }
 }
 
 /// The Command struct will represent an entry in the log
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 enum Cmd {
     Set { key: String, value: String },
     Remove { key: String },
+    Empty,
 }
 
 impl KvStore {
@@ -52,13 +62,18 @@ impl KvStore {
     pub fn set(&mut self, k: String, v: String) -> Result<()> {
         let cmd = Cmd::Set {
             key: k.to_owned(),
-            value: v,
+            value: v.clone(),
         };
         let pos = self.logger.pos;
         serde_json::to_writer(&mut self.logger, &cmd)?;
         self.logger.flush()?;
-        self.index
-            .insert(k.clone(), CmdIdx::new(pos, self.logger.pos - pos));
+        let len = self.logger.pos - pos;
+        self.index.insert(k.clone(), CmdIdx::new(pos, len, cmd));
+        self.uncompacted += len as u64;
+
+        if self.uncompacted > MAX_LOG_UNCOMPACTED_BYTES {
+            self.compact()?;
+        }
         Ok(())
     }
 
@@ -95,9 +110,13 @@ impl KvStore {
         let pos = self.logger.pos;
         serde_json::to_writer(&mut self.logger, &cmd)?;
         self.logger.flush()?;
-        self.index
-            .insert(k, CmdIdx::new(pos, self.logger.pos - pos));
+        let len = self.logger.pos - pos;
+        self.index.insert(k, CmdIdx::new(pos, len, cmd));
+        self.uncompacted += len as u64;
 
+        if self.uncompacted > MAX_LOG_UNCOMPACTED_BYTES {
+            self.compact()?;
+        }
         Ok(())
     }
     /// Open KvStore
@@ -106,6 +125,7 @@ impl KvStore {
         let mut ret = KvStore {
             logger: Logger::new(path)?,
             index: HashMap::new(),
+            uncompacted: 0,
         };
 
         // reconstruct the index
@@ -116,14 +136,20 @@ impl KvStore {
         // for cmd in reader {
         while let Some(cmd) = reader.next() {
             let len = reader.byte_offset() - read_pos;
-            let cmd_idx = CmdIdx::new(read_pos, len);
+            let mut cmd_idx = CmdIdx::new(read_pos, len, Cmd::Empty);
             match cmd? {
-                Cmd::Set { key, .. } => {
+                Cmd::Set { key, value } => {
+                    cmd_idx.cmd = Cmd::Set {
+                        key: key.clone(),
+                        value,
+                    };
                     ret.index.insert(key.clone(), cmd_idx);
                 }
                 Cmd::Remove { key } => {
+                    cmd_idx.cmd = Cmd::Remove { key: key.clone() };
                     ret.index.insert(key.clone(), cmd_idx);
                 }
+                Cmd::Empty => return Err(KvsError::UnexpectedCommandType.into()),
             }
             read_pos += len;
         }
@@ -133,9 +159,50 @@ impl KvStore {
 
         Ok(ret)
     }
+
+    fn compact(&mut self) -> Result<()> {
+        // dump self.index into new file and reset the writer
+        let old_log = self.logger.filename.clone();
+        let new_log = old_log
+            .parent()
+            .ok_or(KvsError::UnexpectedCommandType)?
+            .join(format!("{}.log", Uuid::new_v4().to_string()));
+        self.logger.writer = BufWriter::new(get_file_handler(&new_log)?);
+
+        let mut new_index: HashMap<String, CmdIdx> = HashMap::new();
+        let mut pos = 0;
+        for (k, cmdidx) in self.index.iter() {
+            let cmd = &cmdidx.cmd;
+            serde_json::to_writer(&mut self.logger, &cmd)?;
+            self.logger.flush()?;
+            let len = self.logger.pos - pos;
+            new_index.insert(k.to_string(), CmdIdx::new(pos, len, cmd.clone()));
+            pos += len;
+        }
+        // reset the reader to this new file
+        self.logger.reader = BufReader::new(get_file_handler(&new_log)?);
+        self.index = new_index;
+
+        self.logger.pos = pos;
+        self.uncompacted = 0;
+
+        // remember to remove the file
+        println!("About to remove: {}", old_log.to_str().unwrap());
+        std::fs::remove_file(&old_log)?;
+        Ok(())
+    }
 }
 
 fn get_file_handler(path: impl Into<PathBuf>) -> Result<File> {
+    Ok(OpenOptions::new()
+        .write(true)
+        .read(true)
+        .append(true)
+        .create(true)
+        .open(path.into())?)
+}
+
+fn get_log_name(path: impl Into<PathBuf>) -> Result<PathBuf> {
     // If there was no log file in the `path` directory, create one with uuid file name.
     // Else we reuse the existing file.
     let pathbuf = path.into();
@@ -161,18 +228,13 @@ fn get_file_handler(path: impl Into<PathBuf>) -> Result<File> {
     };
 
     if read_dir(&pathbuf).is_err() {
-        create_dir(&pathbuf)?
+        create_dir(&pathbuf)?;
     }
-
-    Ok(OpenOptions::new()
-        .write(true)
-        .read(true)
-        .append(true)
-        .create(true)
-        .open(file_name)?)
+    Ok(file_name)
 }
 
 struct Logger {
+    filename: PathBuf,
     writer: BufWriter<File>,
     reader: BufReader<File>,
     pos: usize,      // current curson in the file
@@ -180,9 +242,11 @@ struct Logger {
 }
 impl Logger {
     fn new(path: impl Into<PathBuf> + std::marker::Copy) -> Result<Self> {
+        let filename = get_log_name(path)?;
         Ok(Logger {
-            writer: BufWriter::new(get_file_handler(path)?),
-            reader: BufReader::new(get_file_handler(path)?),
+            filename: filename.clone(),
+            writer: BufWriter::new(get_file_handler(&filename)?),
+            reader: BufReader::new(get_file_handler(&filename)?),
             pos: 0,
             read_pos: 0,
         })
